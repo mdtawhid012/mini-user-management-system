6b429b84fc6abffedd88831e376d0ed5265bb378
Tawhid <mohammedtawhid12@gmail.com>
2025-12-29 19:59:15 +0530

    update logout implementation

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { logout, signin, signup } from "../controllers/auth.controller.ts";
+import { authMiddleware } from "../middleware/auth.middleware.ts";
 
 const authRouter = Router();
 
 authRouter.post("/signup", signup);
 authRouter.post("/signin", signin);
-authRouter.post("/logout", logout);
+authRouter.post("/logout", authMiddleware, logout);
 
 export default authRouter;
