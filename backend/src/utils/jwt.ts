c2d1380c046cab1dbbcf376448d4c6d200a6621e
Tawhid <mohammedtawhid12@gmail.com>
2025-12-29 19:57:46 +0530

    update jwt authentication

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,6 +1,10 @@
-import jwt from "jsonwebtoken";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
+
+interface AuthPayload extends JwtPayload {
+  userId: string;
+}
 
 export const generateToken = (userId: string) => {
   return jwt.sign({ userId }, JWT_SECRET);
@@ -8,7 +12,7 @@ export const generateToken = (userId: string) => {
 
 export const verifyToken = (token: string) => {
   try {
-    return jwt.verify(token, JWT_SECRET);
+    return jwt.verify(token, JWT_SECRET) as AuthPayload;
   } catch (error) {
     throw new Error("Invalid or expired token");
   }
