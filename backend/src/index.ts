e540ad95a4cb088f7b9c2774180be0db0ecbe88e
Tawhid <mohammedtawhid12@gmail.com>
2025-12-31 09:18:26 +0530

    update backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,10 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on PORT: ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
 
 export default app;
